2ab62a2f0be0e4bc6c37beb12eae3c95928b14aa
joan purba <88446736+joanpurbaa@users.noreply.github.com>
2024-08-11 17:03:14 +0700

    feat : adding plausible

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,7 @@ import TechStack from "./components/TechStack";
 import Experience from "./components/Experience";
 import Activities from "./components/Activities";
 import Projects from "./components/Projects";
+import PlausibleProvider from 'next-plausible'
 
 const Home = () => {
   const customTheme = {
@@ -151,16 +152,18 @@ const Home = () => {
 
   return (
     <>
-      <Flowbite theme={{ theme: customTheme }}>
-        <div id="home" className="background bg-no-repeat bg-cover flex flex-col justify-between gap-y-12 px-2 md:px-5 lg:px-20 xl:px-48 2xl:px-72 py-10">
-          <LandingPage />
-          <About />
-          <TechStack />
-          <Experience />
-          <Activities />
-          <Projects />
-        </div>
-      </Flowbite>
+      <PlausibleProvider domain="https://profil-joan.vercel.app/">
+        <Flowbite theme={{ theme: customTheme }}>
+          <div id="home" className="background bg-no-repeat bg-cover flex flex-col justify-between gap-y-12 px-2 md:px-5 lg:px-20 xl:px-48 2xl:px-72 py-10">
+            <LandingPage />
+            <About />
+            <TechStack />
+            <Experience />
+            <Activities />
+            <Projects />
+          </div>
+        </Flowbite>
+      </PlausibleProvider>
     </>
   );
 };
