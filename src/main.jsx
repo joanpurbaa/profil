a66326a60f191cc7ce9928a1cbc3c82d8e582acb
joanpurbaa <joanpurba562@gmail.com>
2025-05-27 12:52:26 +0700

    feat : add react strict  mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./Home.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Home />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Home />
+	</React.StrictMode>
+);
